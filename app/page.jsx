1900c3678a50df33b2b3692119c738b0fe025306
Mansur Rahamathullah <fre2mansur@gmail.com>
2024-10-02 03:30:00 +0530

    lock update

--- app/page.jsx
+++ app/page.jsx
@@ -33,8 +33,20 @@ export default function Home() {
   const hexOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
   const initialValues = new Array(40).fill("0");
   initialValues[0] = 1;
+
+  const initiallockValues = new Array(40).fill(0);
+  const [lockValues, setLockValues] = useState(initiallockValues);
+
   const [values, setValues] = useState(initialValues);
   const [publicKey, setPublicKey] = useState("000000000000000000000000000000000000000000000000000000000000000000");
+
+  const lockIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+</svg>;
+
+  const unlockIcon =  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+</svg>;
 
   const handleSelectChange = (index, event) => {
     const newValues = [...values];
@@ -47,8 +59,15 @@ export default function Home() {
     if(target == publicKey) {
       return false;
     }
-   // const newValues = values.map((val) => (val === "0" ? getRandomHexValue() : val));
-   const newValues = values.map((val) => ( getRandomHexValue()));
+   //const newValues = values.map((val) => (val === "0" ? getRandomHexValue() : val));
+   const newValues = values.map((val, index) => {
+    // Only update if lockValues[index] is 0 and the value is "0"
+    if (lockValues[index] === 0) {
+      return getRandomHexValue();
+    } else {
+      return val; // Keep the current value if locked or non-zero
+    }
+  });
    
     setValues(newValues);
     setPublicKey(generatePublicKey(newValues));
@@ -71,14 +90,28 @@ export default function Home() {
     }
   };
 
+  const lock = (index) => {
+    const newLockValues = [...lockValues];
+    newLockValues[index] = 1;
+    setLockValues(newLockValues);
+    console.log(lockValues);
+  }
+
+  const unlock = (index) => {
+    const newLockValues = [...lockValues];
+    newLockValues[index] = 0;
+    setLockValues(newLockValues);
+    console.log(lockValues);
+  }
+
   return (
     <div style={{ padding: "20px" }}>
 
       <h1>Hexadecimal Input</h1>
       <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
         {values.map((value, index) => (
-          <>
-          <select
+          <div class="flex bg-gray-100 space-x-2 items-center">
+          <select disabled = {lockValues[index] == 1 ? true: false}
             key={index}
             value={value}
             onChange={(e) => handleSelectChange(index, e)}
@@ -90,8 +123,10 @@ export default function Home() {
               </option>
             ))}
           </select>
-         
-          </>
+           {lockValues[index] == 0 ? <div onClick={()=>{lock(index)}}>{unlockIcon}</div> : <div onClick={()=>{unlock(index)}}>{lockIcon}</div>}
+            
+            
+          </div>
         ))}
       </div>
       <div style={{ marginTop: "20px" }}>
